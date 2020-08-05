b2f02108f9dbe6ba9a75d30a118b26611ac3e8d8
Michał Tomsia <michal.tomsia@xsolve.pl>
2020-08-05 11:51:34 +0200

    improvement(core): allow defining included resources on response object

--- src/jsonApiResponse.builder.ts
+++ src/jsonApiResponse.builder.ts
@@ -1,6 +1,7 @@
 import { JsonApiResponse, JsonApiResponseData } from './jsonApiResponse.interface';
 import { JsonApiLink } from './jsonApiLinks.interface';
 import { JsonApiLinksBuilder } from './jsonApiLinks.builder';
+import { JsonApiResource } from './jsonApiResource.interface';
 
 export class JsonApiResponseBuilder<T = {}> {
   private data: T = {} as any;
@@ -18,6 +19,11 @@ export class JsonApiResponseBuilder<T = {}> {
 
   public withMeta<U extends object>(meta: U): JsonApiResponseBuilder<T & { meta: U }> {
     Object.assign(this.data, { meta });
+    return this as any;
+  }
+
+  public withIncluded<U extends JsonApiResource[]>(included: U): JsonApiResponseBuilder<T & { included: U }> {
+    Object.assign(this.data, { included });
     return this as any;
   }
 
